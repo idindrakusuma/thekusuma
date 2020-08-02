655f27d0d0f43c46046904b9fadfad82356a528d
Indra Kusuma <indrakusuma.udinus@gmail.com>
2020-08-02 13:29:26 +0700

    feat: add QR generator

---
+++ src/pages/generate-qr.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import MainLayout from '@components/Layout';
+import QRCard from '@components/QRCard';
+import useGuestData from '@/hooks/useGuestData';
+import { styWrapper } from '@components/QRCard/styles';
+
+function GenerateQRPage() {
+  const { data, loading } = useGuestData();
+
+  const renderContent = () => {
+    if (loading) return <div>Menyiapkan data..</div>;
+
+    return data.map((guest) => {
+      return <QRCard name={guest.name} code={guest.code} />;
+    });
+  };
+
+  return (
+    <MainLayout>
+      <div css={styWrapper}>
+        <div className="row">{renderContent()}</div>
+      </div>
+    </MainLayout>
+  );
+}
+
+export default GenerateQRPage;
