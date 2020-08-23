640aaec5345088ade787b9e0ab27a2bbbcc46535
Indra Kusuma <indrakusuma.udinus@gmail.com>
2020-08-23 20:33:48 +0700

    feat: add some basic meta tags info

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,6 +7,9 @@ import '@assets/css/icomoon.css';
 import '@assets/css/bootstrap.css';
 import '@assets/css/style.css';
 
+const IMAGE_URL = `https://thekusuma.com/static/slide-6-4715e29302dbaa2ba21494c6258298d4.jpg`;
+const META_DESCRIPTION = `Dengan memohon Rahmat dan Ridho Illahi, teriring niat menjalankan Sunnah Rasulullah ﷺ untuk membentuk rumah tangga yang Sakinah, Mawaddah wa Rahmah, kami mohon do'a agar senantiasa diberikan kelancaran dan keberkahan. - Dinda & Indra`;
+
 function MainLayout({ children }) {
   return (
     <Fragment>
@@ -17,15 +20,17 @@ function MainLayout({ children }) {
         <link rel="icon" type="image/png" href={Favicon} />
 
         {/* font and SEO tags */}
-        <meta property="og:title" content="" />
-        <meta property="og:image" content="" />
-        <meta property="og:url" content="" />
-        <meta property="og:site_name" content="" />
-        <meta property="og:description" content="" />
-        <meta name="twitter:title" content="" />
-        <meta name="twitter:image" content="" />
-        <meta name="twitter:url" content="" />
-        <meta name="twitter:card" content="" />
+        <meta property="og:title" content="The Wedding of Dinda & Indra" />
+        <meta property="og:image" content={IMAGE_URL} />
+        <meta property="og:url" content="https://thekusuma.com" />
+        <meta property="og:site_name" content="The Wedding of Dinda & Indra" />
+        <meta property="og:description" content={META_DESCRIPTION} />
+        <meta name="twitter:title" content="The Wedding of Dinda & Indra" />
+        <meta name="twitter:description" content={META_DESCRIPTION} />
+        <meta name="twitter:image" content={IMAGE_URL} />
+        <meta name="twitter:url" content="https://thekusuma.com" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="@idindrakusuma" />
 
         <link
           href="https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700"
