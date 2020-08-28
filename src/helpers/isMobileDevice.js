121f6ed21d89b273c6c921de5e985a284696e2ef
AggieWB <usmaggie23@gmail.com>
2020-08-28 07:49:54 -0700

    Update if statements to use short circuit evaluation

--- src/helpers/isMobileDevice.js
+++ src/helpers/isMobileDevice.js
@@ -4,13 +4,9 @@ function isMobileDevice() {
   const userAgent = window.navigator.userAgent || '';
   const finalUserAgent = userAgent.toLocaleLowerCase();
 
-  if (finalUserAgent.includes('android')) return true;
-  if (finalUserAgent.includes('ios')) return true;
-  if (finalUserAgent.includes('iphone')) return true;
-  if (finalUserAgent.includes('windows phone')) return true;
-  if (finalUserAgent.includes('bb')) return true;
-
-  return false;
+  return finalUserAgent.includes('android') || finalUserAgent.includes('ios') || 
+         finalUserAgent.includes('iphone') || finalUserAgent.includes('windows phone') ||
+         finalUserAgent.includes('bb');
 }
 
 export default isMobileDevice;
