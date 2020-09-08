2b850e84ad08e9a32ec0d77d4a63c02f17ac2d0c
Indra Kusuma <indrakusuma.udinus@gmail.com>
2020-09-08 20:28:45 +0700

    feat: add more wishes from friends

--- src/components/WishesSection/wishlist-data.js
+++ src/components/WishesSection/wishlist-data.js
@@ -20,6 +20,9 @@ import yunita from './assets/w-yunita.jpg';
 import fitria from './assets/w-fitria.jpg';
 import joshua from './assets/w-joshua.jpg';
 import sibra from './assets/w-sibra.jpg';
+import irfan from './assets/w-irfan-m.png';
+import reza from './assets/w-reza.jpg';
+import jenny from './assets/w-jenny.jpg';
 
 export const wishlist = [
   {
@@ -179,5 +182,25 @@ export const wishlist = [
     infoName: 'Rekan Kerja Indra, Tokopedia',
     description: `Selamat membuka lembaran baru indra dan dinda, semoga menjadi keluarga yang harmonis dan saling menguatkan di masa sulit maupun senang.. congrats bro indra..`,
     image: sibra,
+  },
+  {
+    name: 'Reza Maskadi',
+    infoName: 'Rekan Kerja Indra, Tokopedia',
+    description:
+      "Selamat menikah untuk Indra dan Dinda, teruslah untuk 'saling', saling menjaga, saling memberi semangat, saling percaya, saling melengkapi, saling mengerti, serta saling saling yang akan kalian temui kedepannya, walau suka dan duka tetaplah jaga `saling` tersebut. Juga semoga menjadi keluarga sakinah mawaddah warrahmah. amin...",
+    image: reza,
+  },
+  {
+    name: 'Irfan Maulana',
+    infoName: 'Rekan Kerja Indra, Tokopedia',
+    description: `"Selamat berbahagia, semoga semakin berkah kehidupannya dan bisa saling melengkapi satu dengan yang lain.
+      Semoga pernikahannya bisa dilanggengkan."`,
+    image: irfan,
+  },
+  {
+    name: 'Jennyka Setiawan',
+    infoName: 'Sahabat Kuliah Dinda',
+    description: `Dinda dan Indra When you read this message you probably already "Sah" Newly Married couple. or if you read this before that then read this again after. hehehehehee... Dinda and Indra you already are match couple 2020, so keep up the good work until only death takes youu guys apart. I definitely know nothing about marriage but people says its the bittersweet of life. But I know one thing you guys can be each other the best of friends, parents, leader, guidance, partner not in crime but in the good thing I know ( crime only can be done by Dinda while in university ) ehehhehehe. Hope you guys prosper and also can be the source to share good value not only for your family but also for the lots and lots of people. May God Bless You both and your "soon to be" family.  Regard - Jeka0ne`,
+    image: jenny,
   },
 ];
