bebe3e8e506cb42005453527344f3c64832be105
Muhammad Hakim Setyobudi <hakimlogorg@yahoo.co.id>
2022-08-31 01:42:17 +0700

    Update ButtonLive.js

--- src/components/WeddingSection/ButtonLive.js
+++ src/components/WeddingSection/ButtonLive.js
@@ -3,4 +3,10 @@ import React from 'react';
 import IconIg from './assets/instagram.png';
 import { styButtonWrapper } from './styles';
 
+function ButtonLive() {
+  return (
 
+  );
+}
+
+export default React.memo(ButtonLive);
