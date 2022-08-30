87768df407b7f3af89fc5bdef64b95db02e7d757
Muhammad Hakim Setyobudi <hakimlogorg@yahoo.co.id>
2022-08-31 01:43:31 +0700

    Update ButtonLive.js

--- src/components/WeddingSection/ButtonLive.js
+++ src/components/WeddingSection/ButtonLive.js
@@ -5,7 +5,19 @@ import { styButtonWrapper } from './styles';
 
 function ButtonLive() {
   return (
-
+    <div className="row">
+      <div className="col-md-12">
+        <p className="text__live">Hadir secara virtual melalui siaran langsung instagram: </p>
+        <a href="https://www.instagram.com/dindadipoyono/" target="_blank" rel="noreferrer">
+          <div css={styButtonWrapper}>
+            <div className="img__wrapper">
+              <img src={IconIg} alt="Live IG" />
+              <span>@dindadipoyono</span>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
   );
 }
 
