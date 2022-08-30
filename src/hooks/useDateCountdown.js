33d2b770205e92d358bfb8450ffb71d71ec6a9d4
Muhammad Hakim Setyobudi <hakimlogorg@yahoo.co.id>
2022-08-31 02:01:21 +0700

    Update useDateCountdown.js

--- src/hooks/useDateCountdown.js
+++ src/hooks/useDateCountdown.js
@@ -23,7 +23,7 @@ function useDateCountdown() {
 
     if (TIME_LEFT < 0) {
       setIsEventOver(currentEpochTime > EPOCH_END_EVENT);
-      setTimeHasRunOut(true);
+      setTimeHasRunOut(false);
       return;
     }
 
