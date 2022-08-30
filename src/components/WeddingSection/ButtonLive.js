09df95a8d393d9f1f62a7409ac9598763f8654b1
Muhammad Hakim Setyobudi <hakimlogorg@yahoo.co.id>
2022-08-31 01:43:52 +0700

    Update ButtonLive.js

--- src/components/WeddingSection/ButtonLive.js
+++ src/components/WeddingSection/ButtonLive.js
@@ -4,21 +4,7 @@ import IconIg from './assets/instagram.png';
 import { styButtonWrapper } from './styles';
 
 function ButtonLive() {
-  return (
-    <div className="row">
-      <div className="col-md-12">
-        <p className="text__live">Hadir secara virtual melalui siaran langsung instagram: </p>
-        <a href="https://www.instagram.com/dindadipoyono/" target="_blank" rel="noreferrer">
-          <div css={styButtonWrapper}>
-            <div className="img__wrapper">
-              <img src={IconIg} alt="Live IG" />
-              <span>@dindadipoyono</span>
-            </div>
-          </div>
-        </a>
-      </div>
-    </div>
-  );
+
 }
 
 export default React.memo(ButtonLive);
