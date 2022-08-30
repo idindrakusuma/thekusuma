cf18754c028091e1240ec7b52cb7356b1e3192b3
Muhammad Hakim Setyobudi <hakimlogorg@yahoo.co.id>
2022-08-31 01:53:43 +0700

    Update CountContainer.js

--- src/components/WelcomeSection/CountContainer.js
+++ src/components/WelcomeSection/CountContainer.js
@@ -1,1 +1,29 @@
+import React from 'react';
+import useDateCountdown from '@/hooks/useDateCountdown';
+import CountItem from './CountItem';
+import ButtonLive from '../WeddingSection/ButtonLive';
+import { styMargin } from './styles';
 
+function CountContainer() {
+  const { days, hours, minutes, seconds, timeHasRunOut, isEventOver } = useDateCountdown();
+  const finalText = isEventOver ? 'SUDAH' : 'SEDANG';
+
+  if (timeHasRunOut)
+    return (
+      <>
+        <div className="row">
+          <div className="col-md-12" style={{ fontSize: '20px' }}>
+           
+          </div>
+        </div>
+        <ButtonLive />
+      </>
+    );
+
+  return (
+    <div className="col-md-12" css={styMargin('0 0 16px 0')}>
+    </div>
+  );
+}
+
+export default CountContainer;
