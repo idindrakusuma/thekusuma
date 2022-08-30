ef96251d5d193bd26fe5f34682ea9adc3be4da2b
Muhammad Hakim Setyobudi <hakimlogorg@yahoo.co.id>
2022-08-31 01:24:51 +0700

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 /**
  * Link
  */
-export const GOOGLE_CALENDAR_LINK = `https://calendar.google.com/calendar/event?action=TEMPLATE&dates=20201003T040000Z%2F20201003T060000Z&text=Dinda+%26+Indra+Wedding&details=Dinda+%26+Indra+Wedding`;
+export const GOOGLE_CALENDAR_LINK = `https://calendar.google.com/calendar/event?action=TEMPLATE&dates=20220924T040000Z%2F20220924T060000Z&text=Dinda+%26+Indra+Wedding&details=Dinda+%26+Indra+Wedding`;
 export const GOOGLE_MAPS_LINK = `https://g.page/HARRISHotelSentralandSemarang?share`;
 
 /**
