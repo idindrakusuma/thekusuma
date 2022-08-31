06082ff22d9180854cdc696555d1bf2c4a048771
Muhammad Hakim Setyobudi <hakimlogorg@yahoo.co.id>
2022-09-01 00:39:09 +0700

    ok

--- src/components/HelloSection/index.js
+++ src/components/HelloSection/index.js
@@ -56,26 +56,6 @@ function HelloSection({ isInvitation }) {
           </div>
         </div>
       </div>
-      {!isInvitation ? (
-        <div id="fh5co-couple" className="fh5co-section-gray" css={styWrapper}>
-          <div className="container">
-            <div className="row">
-              <div className="col-md-8 col-md-offset-2 text-center fh5co-heading">
-                <h2 className="main-font">Seputar Covid-19</h2>
-                <p className="info">
-                  Berkaitan dengan kondisi pandemik saat ini, keberlangsungan acara kami mengacu pada pedoman
-                  Kementerian Kesehatan, yaitu memberlakukan pembatasan jarak dan pembatasan jumlah tamu undangan.
-                </p>
-                <p className="info">
-                  Maka dari itu, dengan segala kerendahan hati, kami mohon maaf jika belum bisa mengundang saudara/i
-                  dihari bahagia kami. Sekiranya do'a dari saudara/i sudah lebih dari cukup bagi kami. Semoga kita semua
-                  selalu dalam lindungan Allah SWT. Aamiin ya rabbal allamiin..
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      ) : null}
     </Fragment>
   );
 }
--- src/components/WishesSection/index.js
+++ src/components/WishesSection/index.js
@@ -3,22 +3,7 @@ import WishesContainer from './WishesContainer';
 
 function WishesSection() {
   return (
-    <div id="fh5co-testimonial">
-      <div className="container">
-        <div className="row">
-          <div className="row">
-            <div className="col-md-8 col-md-offset-2 text-center fh5co-heading">
-              <h2 className="main-font">Dari Sahabat</h2>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-12">
-              <WishesContainer />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+
   );
 }
 
