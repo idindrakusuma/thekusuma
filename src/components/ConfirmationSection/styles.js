95df2283232a7f9721b85fb29ae3548ffdb97dd8
Muhammad Hakim Setyobudi <hakimlogorg@yahoo.co.id>
2022-09-04 02:34:10 +0700

    ok

--- src/components/ConfirmationSection/styles.js
+++ src/components/ConfirmationSection/styles.js
@@ -1,5 +1,5 @@
 import { css } from '@emotion/core';
-import Background from '@assets/images/cileunca.jpg';
+import Background from '@assets/images/ngguri.jpg';
 
 export const styWrapper = css`
   background-image: url(${Background});
