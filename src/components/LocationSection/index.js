8b1046795c6096fc250d421d5209a94fb2fca619
Muhammad Hakim Setyobudi <hakimlogorg@yahoo.co.id>
2022-09-04 20:54:23 +0700

    Update index.js

--- src/components/LocationSection/index.js
+++ src/components/LocationSection/index.js
@@ -44,7 +44,6 @@ function LocationSection() {
           </div>
           <div className="row">
             <div className="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
-              <h2 className="main-font">Lokasi Acara</h2>
               <p className="sub-title">
                 <a
                   href={GOOGLE_MAPS_LINK}
@@ -59,6 +58,21 @@ function LocationSection() {
                 Jl. Laswi Cangkring RT/RW 05/07
                 kel. Jelekong, kec. Baleendah, kab. Bandung
               </p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-10 col-md-offset-1">
+              <iframe
+                src="https://maps.google.com/maps?q=Klinik%20Bintari%20Medika&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                width="100%"
+                height="450"
+                frameBorder="0"
+                style={{ border: '0' }}
+                allowFullScreen
+                aria-hidden="false"
+                tabIndex="0"
+                title="Google Maps - Eka & Hakim Wedding Party"
+              ></iframe>
             </div>
           </div>
         </div>
